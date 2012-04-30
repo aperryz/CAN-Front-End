ed10d9cd71f3f88cfc89a84a425f41936b24675f
Jonathan Goldford <jonathan.goldford@jgvisual.com>
2012-04-30 18:37:53 -0500

    Further design to html with new illustrator file.  Mostly work on the main pages.

--- index.php
+++ index.php
@@ -8,11 +8,6 @@
           <div class="page-header">
             <h1 data-name="home" data-text="Home" class="dfo-logo">Design for Obama 2012</h1>
           </div>
-          <div class="row">
-            <div class="span12">
-            <p class="align-center"><a href="#" class="link-pop">Is this for me?</a></p>
-            </div>
-          </div>
         </div>
 
         <div class="page">
@@ -22,14 +17,22 @@
           <div class="row">
             <div class="span12">
               <div class="row">
-                <div class="span4">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="span12">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium Doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. isquavoluptatem. Ut enim ad minima veniam, que consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
                 </div>
-                <div class="span4">
-                  <p>Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+              </div>
+              <div class="row">
+                <div class="span4 count">
+                  <span class="num" data-number="757"></span>
+                  Posters Aggregated
                 </div>
-                <div class="span4">
-                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                <div class="span4 count">
+                  <span class="num" data-number="306"></span>
+                  Participating Artists
+                </div>
+                <div class="span4 count">
+                  <span class="num" data-number="62"></span>
+                  Online Poster Views
                 </div>
               </div>
             </div>
@@ -38,24 +41,53 @@
 
         <div class="page">
           <div class="page-header">
-            <h1 data-name="the-book" data-text="The Book">The Book</h1>
+            <h1 data-name="the-book" data-text="The Book">Design for Obama</h1>
+            <h2>POSTERS FOR CHANGE - A GRASSROOTS ANTHOLOGY</h2>
+            <h3>Edited by Spike LEE and Aaron perry-zucker, essay by steven heller</h3>
+          </div>
+          <div class="row">
+            <div class="span3">
+              <img src="img/dfo/book/design-for-obama-book-closed.jpg" width="225"  height="225" alt="Design for Obama Book" title="Design for Obama Book Closed" />
+            </div>
+            <div class="span3">
+              <img src="img/dfo/book/design-for-obama-book-gallery.jpg" width="225"  height="225" alt="Design for Obama Book" title="Design for Obama Book Gallery" />
+            </div>
+            <div class="span3">
+              <img src="img/dfo/book/design-for-obama-book-right-thing.jpg" width="225"  height="225" alt="Design for Obama Book" title="Design for Obama Book - Do the Right Thing" />
+            </div>
+            <div class="span3">
+              <img src="img/dfo/book/design-for-obama-book-new-game.jpg" width="225"  height="225" alt="Design for Obama Book" title="Design for Obama Book - New Game" />
+            </div>
+          </div>
+          <div class="row book-links align-center">
+            <div class="span3 first">
+              <a href="http://www.whitehouse.gov/" target="_blank">External Link 1</a>
+            </div>
+            <div class="span3">
+              <a href="http://www.barackobama.com/" target="_blank">External Link 2</a>
+            </div>
+            <div class="span3">
+              <a href="http://designforobama.org/" target="_blank">External Link 3</a>
+            </div>
+            <div class="span3">
+              <a href="http://www.amazon.com/Design-Obama-Posters-Grassroots-Anthology/dp/3836518562" target="_blank">External Link 4</a>
+            </div>
           </div>
           <div class="row">
             <div class="span12">
-              <p>Morbi ac tortor ac augue sollicitudin cursus. Nunc sagittis, ipsum quis congue venenatis, tortor sapien vulputate ante, in hendrerit mi risus eu augue. Nam sed ultricies sem. Quisque pharetra vestibulum nibh et faucibus. Aliquam faucibus pellentesque massa non venenatis. Nunc tristique ante vel diam rhoncus nec eleifend est mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget mauris vel arcu ullamcorper aliquet. Sed tempus posuere mauris non sagittis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.</p>
             </div>
           </div>
         </div>
 
         <div class="page">
           <div class="page-header">
-            <h1 data-name="contributing" data-text="Contributing">Contributing</h1>
+            <h1 data-name="contributing" data-text="Contributing">Contributing (This Needs Love)</h1>
           </div>
           <div class="row">
             <div class="span12">
-              <p>Morbi ac tortor ac augue sollicitudin cursus. Nunc sagittis, ipsum quis congue venenatis, tortor sapien vulputate ante, in hendrerit mi risus eu augue. Nam sed ultricies sem. Quisque pharetra vestibulum nibh et faucibus. Aliquam faucibus pellentesque massa non venenatis. Nunc tristique ante vel diam rhoncus nec eleifend est mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget mauris vel arcu ullamcorper aliquet. Sed tempus posuere mauris non sagittis.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium Doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. isquavoluptatem. Ut enim ad minima veniam, que consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-              <p>Morbi ac tortor ac augue sollicitudin cursus. Nunc sagittis, ipsum quis congue venenatis, tortor sapien vulputate ante, in hendrerit mi risus eu augue. Nam sed ultricies sem. Quisque pharetra vestibulum nibh et faucibus. Aliquam faucibus pellentesque massa non venenatis. Nunc tristique ante vel diam rhoncus nec eleifend est mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget mauris vel arcu ullamcorper aliquet. Sed tempus posuere mauris non sagittis.</p>
+              <p>This content still needs to be created.</p>
               <div class="row">
                 <div class="span6 offset3">
                   <button class="btn btn-large" data-toggle="modal" data-target="#submission-modal">Submit</button>
