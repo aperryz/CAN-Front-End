5643aff15fa94a9b3bf604cde7736609833aa18a
Jonathan Goldford <jonathan.goldford@jgvisual.com>
2012-05-02 18:19:59 -0500

    Started building profile in profile.php.

---
+++ profile.php
@@ -0,0 +1,36 @@
+<?php $page_class = 'profile'; //Used to add a body class to each page.  ?>
+<?php include_once 'includes/header.php'; ?> 
+<?php include_once 'includes/navigation.php'; ?>
+
+<div id="pages-container">
+  <div class="container">
+    <div id="pages">
+      <div class="page">
+        <div id="user-info">
+          <img src="FILL IN DUDE" height="100" width="100" alt="Add Profile Photo" title="Add Profile Photo" />
+          
+          <div id="user-content">
+            <h1>Spizike Lee</h1>
+            <div id="bio">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies suscipit interdum.
+                In volutpat bibendum suscipit. Praesent ac dui leo, id faucibus ipsum. Vivamus justo metus, lacinia nec sodales at, rhoncus vitae felis.
+                Vestibulum turpis erat, rutrum ac viverra eget, pharetra et neque.</p>
+            </div>
+            <div class="circle-nav">
+              <a href="#">&nbsp;</a>
+              <a href="#">&nbsp;</a>
+              <a href="#">&nbsp;</a>
+              <a href="#">&nbsp;</a>
+            </div>
+          </div> 
+          
+        </div>
+      </div>
+    </div>
+  </div><!-- End .container div -->
+</div><!-- End #pages-container div -->
+
+
+<div class="container">
+<?php //include_once 'includes/grid.php'; ?>
+<?php include_once 'includes/footer.php'; ?>
